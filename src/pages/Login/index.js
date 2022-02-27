81cf48a227dd9652d9a109b91ad49865fe8306f3
Biruk Endris <57604289+Biruk-hub@users.noreply.github.com>
2022-02-27 18:40:14 +0100

    Login and register page implemented

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,12 +1,45 @@
-import React from 'react'
-import PageWrapper from '../../components/page_wrapper'
+import React from "react";
+import PageWrapper from "../../components/page_wrapper";
 
 const LoginPage = () => {
   return (
     <PageWrapper>
-        <h1>Login</h1>
-    </PageWrapper>
-  )
-}
+      <div className="flex justify-center items-center">
+        <div>
+          <h1 className="text-4xl mb-3 font-bold">Login</h1>
 
-export default LoginPage
+          <div className="relative mb-4">
+            <label
+              htmlFor="full-name"
+              className="leading-7 text-sm text-gray-600"
+            >
+              Username
+            </label>
+            <input
+              type="username"
+              id="username"
+              name="username"
+              className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div className="relative mb-4">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-600">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <button className="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">
+            Login
+          </button>
+        </div>
+      </div>
+    </PageWrapper>
+  );
+};
+
+export default LoginPage;
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -4,7 +4,54 @@ import PageWrapper from '../../components/page_wrapper'
 const RegisterPage = () => {
   return (
     <PageWrapper>
-        <h1>Register</h1>
+        <div className="flex justify-center items-center">
+        <div>
+          <h1 className="text-4xl mb-3 font-bold">Register</h1>
+
+          <div className="relative mb-4">
+            <label
+              htmlFor="full-name"
+              className="leading-7 text-sm text-gray-600"
+            >
+              Full name
+            </label>
+            <input
+              type="text"
+              id="full-name"
+              name="full-name"
+              className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div className="relative mb-4">
+            <label
+              htmlFor="Username"
+              className="leading-7 text-sm text-gray-600"
+            >
+              Username
+            </label>
+            <input
+              type="username"
+              id="username"
+              name="username"
+              className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div className="relative mb-4">
+            <label htmlFor="Password" className="leading-7 text-sm text-gray-600">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="w-full bg-white rounded border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <button className="text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">
+            Create Account
+          </button>
+        </div>
+      </div>
     </PageWrapper>
   )
 }
